d9df9ff30b358614e4a8560238895097a10f57cd
Artem Zhyto <artemzhr11@gmail.com>
2026-05-04 17:50:27 +0300

    frontend init

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,15 @@
+// Styles
+import '../global.css'
+
+// Types
+import { LocalesLayoutT } from '@shared-types/layouts'
+
+const RootLayout = async ({ children }: LocalesLayoutT) => {
+  return (
+    <html lang='en-US'>
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ frontend/src/app/quizzes/[quizID]/page.tsx
@@ -0,0 +1,5 @@
+const Quiz = (params: string) => {
+  return <div>Quiz</div>
+}
+
+export default Quiz
